3664651168d471d62b88aec3ae4d25eedc74ed99
Calypso Leonard <calypsorleonard@gmail.com>
2022-06-16 18:31:08 -0400

    commit tree 328a94aa832f27a9945833ec3953b51e3a8d1d41

--- lib/fix_the_string.rb
+++ lib/fix_the_string.rb
@@ -1,1 +1,24 @@
 # your code here
+
+def fix_the_string string
+    fixed = ""
+
+    stack = []
+
+    string.each_char do |char|
+        if !stack.empty? && char != stack.last && char.upcase == stack.last.upcase
+            stack.pop
+        else
+            stack.push(char)
+        end
+    end
+
+    fixed = stack.pop + fixed until stack.empty?
+    fixed
+
+    # for letter in string
+    #     if 
+            
+    #     end
+    # end
+end
